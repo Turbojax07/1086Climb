8e96d35dc2e43194e3e46a66e4036585b113f417
turbojax07 <turbojax07@gmail.com>
2025-04-04 16:43:07 -0400

    Removing unchanged functions

--- commands/SetClimbAngle.java
+++ commands/SetClimbAngle.java
@@ -28,17 +28,9 @@ public class SetClimbAngle extends Command {
         climb.setAngle(angle);
     }
 
-    /** Called every time the scheduler runs while the command is scheduled. */
-    @Override
-    public void execute() {}
-
     /** Returns true when the command should end. */
     @Override
     public boolean isFinished() {
         return true;
     }
-
-    /** Called once the command ends or is interrupted. */
-    @Override
-    public void end(boolean interrupted) {}
 }
